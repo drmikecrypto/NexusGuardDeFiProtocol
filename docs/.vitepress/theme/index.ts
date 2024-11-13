1285cdc2d3e5d21976a397f063969108f6013986
dr mike <91358784+drmikecrypto@users.noreply.github.com>
2024-11-13 22:27:33 +0330

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,22 @@
+// docs/.vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import { type Theme } from 'vitepress'
+import Layout from './Layout.vue'
 import Roadmap from './components/Roadmap.vue'
 import Partners from './components/Partners.vue'
 import Features from './components/Features.vue'
-import './styles/custom.css'
+import ProtocolMetrics from './components/ProtocolMetrics.vue'
+import CodeBlock from './components/CodeBlock.vue'
+import './styles/main.css'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component('Roadmap', Roadmap)
     app.component('Partners', Partners)
     app.component('Features', Features)
+    app.component('ProtocolMetrics', ProtocolMetrics)
+    app.component('CodeBlock', CodeBlock)
   }
-}
+} satisfies Theme
