9ebefdef73db9f4989b7205dfc9cd42adb81ae29
dr mike <91358784+drmikecrypto@users.noreply.github.com>
2024-11-16 07:42:56 +0330

    Delete contracts directory

--- contracts/NexusGuardInsurance.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "./NexusGuardStorage.sol";
-
-abstract contract NexusGuardInsurance is NexusGuardStorage {
-    // Rest of the insurance contract code...
-}
